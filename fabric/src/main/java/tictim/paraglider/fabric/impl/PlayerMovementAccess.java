b0269fc1c6a9ef2e5ce8d56e3b7ec419ecff10f5
SoOvey <tryscaremeifyoucan@gmail.com>
2025-01-07 18:44:07 +0300

    Богу стыдно за фабрик. А его покорный слуга его удалил из проекта

--- fabric/src/main/java/tictim/paraglider/fabric/impl/PlayerMovementAccess.java
+++
@@ -1,8 +0,0 @@
-package tictim.paraglider.fabric.impl;
-
-import org.jetbrains.annotations.NotNull;
-import tictim.paraglider.impl.movement.PlayerMovement;
-
-public interface PlayerMovementAccess{
-	@NotNull PlayerMovement paragliderPlayerMovement();
-}
